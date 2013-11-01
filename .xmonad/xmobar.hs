e26d2b3945cb8f0f1308b745003948e60b77e5cc
Niko Nevala <niko@nevala.fi>
2013-11-01 23:50:37 +0000

    fix xmobar plugin configuration

--- .xmonad/xmobar.hs
+++ .xmonad/xmobar.hs
@@ -1,25 +1,22 @@
--- xmobar config used by Vic Fryzel
--- Author: Vic Fryzel
--- http://github.com/vicfryzel/xmonad-config
-
--- This is setup for dual 1920x1080 monitors, with the right monitor as primary
 Config {
---    font = "xft:Fixed-12",
     font = "xft:Droid Sans Mono:size=10:antialias=true",
     bgColor = "#000000",
     fgColor = "#ffffff",
-    position = Static { xpos = 0, ypos = 0, width = 1800, height = 22 },
+    position = Top,
     lowerOnStart = True,
     commands = [
---        Run Weather "KPAO" ["-t","<tempF>F <skyCondition>","-L","64","-H","77","-n","#CEFFAC","-h","#FFB6B0","-l","#96CBFE"] 36000,
+        Run Weather "EGLL" ["-t","<tempC>C","-L","5","-H","25","-n","#CEFFAC","-h","#FFB6B0","-l","#96CBFE"] 36000,
         Run MultiCpu ["-t","Cpu: <total0> <total1> <total2> <total3>","-L","30","-H","60","-h","#FFB6B0","-l","#CEFFAC","-n","#FFFFCC","-w","3"] 10,
         Run Memory ["-t","Mem: <usedratio>%","-H","8192","-L","4096","-h","#FFB6B0","-l","#CEFFAC","-n","#FFFFCC"] 10,
---        Run Swap ["-t","Swap: <usedratio>%","-H","1024","-L","512","-h","#FFB6B0","-l","#CEFFAC","-n","#FFFFCC"] 10,
-        Run Network "eth0" ["-t","Net: <rx>, <tx>","-H","200","-L","10","-h","#FFB6B0","-l","#CEFFAC","-n","#FFFFCC"] 10,
-        Run Date "%a %b %_d %l:%M" "date" 10,
+        Run Wireless "wlp4s0" ["-t", "<essid> <quality>"] 10,
+        Run Network "wlp4s0" ["-t","r:<rx>, t:<tx>","-H","200","-L","10","-h","#FFB6B0","-l","#CEFFAC","-n","#FFFFCC"] 10,
+        Run Date "%a %b %_d %k:%M:%S" "date" 10,
+        Run BatteryP ["BAT0"] ["-t", "<left>% (<timeleft>)"] 600,
+        Run DiskU [("/", "/ <free>,"), ("/home", "/home <free>")] [] 10,
+        Run ThermalZone 0 ["-L", "500", "-H", "80", "-h", "#FFB6B0", "-l", "#CEFFAC", "-n", "#FFFFCC"] 30,
         Run StdinReader
     ],
     sepChar = "%",
     alignSep = "}{",
-    template = "%StdinReader% }{ %multicpu%   %memory%   %swap%   %eth0%   <fc=#FFFFCC>%date%</fc>   %KPAO%"
+    template = "%StdinReader% }{ SSD: %disku%   %multicpu%   %memory%   Wifi: %wlp4s0wi% (%wlp4s0%)   Bat: %battery%   Temp: %thermal0%   Out: %EGLL%   <fc=#FFFFCC>%date%</fc>"
 }
